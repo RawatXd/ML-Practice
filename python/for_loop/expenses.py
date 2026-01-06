add926a2dbc71c2e3d4f93b25d8afdac8b029223
Abhishek Rawat <rawatabhishek.123321@gmail.com>
2026-01-06 23:43:32 +0530

    for_loop

---
+++ python/for_loop/expenses.py
@@ -0,0 +1,11 @@
+expenses = [1200,1300,1500,1700]
+
+# total_expenses = 0
+# for expenses in expenses:
+#     total_expenses += expenses
+#
+# print(total_expenses)
+
+for i in range (len(expenses)):
+    expense = expenses[i]
+    print(f"Month {i} , EXPENSES : {expense}")
